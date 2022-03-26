f2599d50056a608c315af885f9ff2f260221e08e
5witchkr <hob4419@gmail.com>
2022-03-27 03:58:44 +0900

    email인증페이지추가

--- src/component/signup.svelte
+++ src/component/signup.svelte
@@ -1,27 +1,34 @@
 
 <script>
     import { fly } from 'svelte/transition';
-    import { signin, signup, mainpage } from "../store/writableStore.js";
+    import { signin, signup, mainpage, emailsend } from "../store/writableStore.js";
   
-
     function signinClick(){
         signin.update(()=>{return true});
         signup.update(()=>{return false});
+        emailsend.update(()=>{return 0});
+      }
+    function emailSendClick(){
+      emailsend.update(joincunt => joincunt+1);
       }
 
     let email = ''
-	let nickname = ''
+    let emailnumber = ''
+    let nickname = ''
     let password = ''
+    let company = false
     let result = null
-	
+
+
 	async function doJoin () {
 		const res = await fetch('http://127.0.0.1:3000/auth/signup', {
             headers:{'Content-Type':'application/json'},
 			method: 'POST',
 			body: JSON.stringify({
 				email,
-				nickname,
-                password
+        password,
+        nickname,
+        company
 			})
 		})
         const json = await res.json()
@@ -32,6 +39,7 @@
         }
 	}
   
+
   </script>
 
 <div in:fly="{{ y: -150, duration: 1000 }}" class="hero min-h-screen bg-base-200" >
@@ -39,10 +47,30 @@
       <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
         <div class="card-body">
           <div class="form-control">
+            <span class="label-text">회원가입 페이지</span>
             <div class="label">
               <span class="label-text">Email</span>
             </div>
+            {#if $emailsend == 0}
             <input type="text" placeholder="email" bind:value={email} class="input input-bordered">
+            {/if}
+            {#if $emailsend >= 1}
+            <input type="text" placeholder="email" bind:value={email} class="input input-bordered" readonly>
+            {/if}
+          </div>
+          {#if $emailsend == 0}
+          <button on:click={emailSendClick} class="btn btn-primary">인증번호 보내기</button>
+          {/if}
+          {#if $emailsend == 1}
+          <input type="text" placeholder="인증번호를 입력해주세요" bind:value={emailnumber} class="input input-bordered">
+          <button on:click={emailSendClick} class="btn btn-primary">인증하기</button>
+          {/if}
+          {#if $emailsend == 2}
+          <div class="form-control">
+            <div class="label">
+              <span class="label-text">Password(4자리 이상)</span>
+            </div>
+            <input type="text" placeholder="password" bind:value={password} class="input input-bordered">
           </div>
           <div class="form-control">
             <div class="label">
@@ -51,16 +79,15 @@
             <input type="text" placeholder="nickname" bind:value={nickname} class="input input-bordered">
           </div>
           <div class="form-control">
-            <div class="label">
-              <span class="label-text">Password (4자리 이상)</span>
-            </div>
-            <input type="text" placeholder="password" bind:value={password} class="input input-bordered">
-            <div class="label">
-              <button on:click={signinClick} class="label-text-alt link link-hover">로그인 하러가기</button>
-            </div>
+            <label class="label cursor-pointer">
+              <span class="label-text">고용주 이신가요?</span> 
+              <input type="checkbox" bind:checked={company} class="checkbox checkbox-primary">
+            </label>
           </div>
+          <button on:click={doJoin} class="btn btn-primary">가입하기</button>
+          {/if}
           <div class="form-control mt-6">
-            <button class="btn btn-primary">회원가입</button>
+            <button on:click={signinClick} class="label-text-alt link link-hover">가입을 이미 하셨나요? 로그인</button>
           </div>
         </div>
       </div>
--- src/store/writableStore.js
+++ src/store/writableStore.js
@@ -3,3 +3,4 @@ import { writable } from "svelte/store";
 export let signin = writable(true);
 export let signup = writable(false);
 export let mainpage = writable(false);
+export let emailsend = writable(0);
